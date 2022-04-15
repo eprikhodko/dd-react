4fd266e8051208d308d6b1c9cd18622454dafdf3
eprikhodko <eprikhodkodev@gmail.com>
2022-04-15 12:40:58 +0300

    feat: create empty form component

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import Header from '../../components/header/header';
-import Filter from '../../components/filter/filter';
-import Board from '../../components/board/board';
-import { AppRoute } from '../../const.js';
+import React from "react";
+import Header from "../../components/header/header";
+import Filter from "../../components/filter/filter";
+import Board from "../../components/board/board";
+import { AppRoute } from "../../const.js";
 
 const Main = () => {
   return (
     <>
-      <Header mode={AppRoute.MAIN}/>
+      <Header mode={AppRoute.MAIN} />
       <section className="main__wrapper">
         <Filter />
         <Board />
       </section>
     </>
-
-  )
-}
+  );
+};
 
 export default Main;
