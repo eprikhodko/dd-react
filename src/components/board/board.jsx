50ab39b79a797df5a11fa41c14db42af085ae99d
eprikhodko <eprikhodkodev@gmail.com>
2022-04-21 14:54:52 +0300

    fix: update board and archive components

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -4,7 +4,6 @@ import { AppRoute } from '../../const';
 import Card from '../card/card';
 import LoadMore from '../load-more/load-more';
 import Sorting from '../sorting/sorting';
-import { AppRoute } from '../../const';
 
 const Board = () => {
   const { pathname } = useLocation();
@@ -12,7 +11,6 @@ const Board = () => {
   return (
     <section className="board">
       {pathname !== AppRoute.ARCHIVE && <Sorting />}
-
 
       <div className="board__events">
         <Card />
--- src/pages/archive/archive.jsx
+++ src/pages/archive/archive.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Board from '../../components/board/board';
 import Header from '../../components/header/header';
+import { AppRoute } from '../../const';
 
 const Archive = (props) => {
   return (
